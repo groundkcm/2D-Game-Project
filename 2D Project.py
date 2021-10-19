bcad487b8d909e53da23d3a5e9e719215f5d3681
groundkcm <karaminseo@naver.com>
2021-10-20 05:16:47 +0900

    inventoy, crash events

--- 2D Project.py
+++ 2D Project.py
@@ -1,7 +1,7 @@
 from pico2d import *
+from time import time
 
 num = 0
-
 def jumping():
     global dir, high
     global num
@@ -23,6 +23,7 @@ click = 0
 def attacking():
     global click
     keys['run'], keys['jump'], keys['defence'] = False, False, False
+
     if click == 1:
         frame['attack'] = (frame['attack'] + 1) % 26
         if frame['attack'] == 7:
@@ -45,11 +46,11 @@ def defencing():
     keys['run'], keys['jump'], keys['attack'] = False, False, False
 
     num += 1
-    dir -= 1
+    dir = -1
     if num == 8:
         keys['defence'] = False
         keys['stop'] = True
-        dir += 1
+        dir = 0
 
 def crash_events():
     global dir, high
@@ -60,45 +61,47 @@ def crash_events():
             mx, my, mw, mh = skeleton1_1['x'], skeleton1_1['y'], skeleton1_1['width'], skeleton1_1['height']
         elif i == "skeleton1_2":
             mx, my, mw, mh = skeleton1_2['x'], skeleton1_2['y'], skeleton1_2['width'], skeleton1_2['height']
-        if x1 + num1 > mx - mw:
-            dir -= 1
+        if mx - mw < x1 + num1 < mx + mw and y1 + num2 > my - mh and y1 - num2 < my + mh:
+            gretel['x'] -= 10
             gretel['Hp'] = gretel['Hp'] - 60
-        elif x1 - num1 < mx + mw:
-            dir += 1
+        elif mx - mw < x1 - num1 < mx + mw and y1 + num2 > my - mh and y1 - num2 < my + mh:
+            gretel['x'] += 10
             gretel['Hp'] = gretel['Hp'] - 60
-        elif y1 + num2 > my - mh:
-            high -= 1
+        elif my - mh < y1 + num2 < my + mh and x1 + num1 > mx - mw and x1 - num1 < mx + mw:
+            gretel['y'] -= 10
             gretel['Hp'] = gretel['Hp'] - 60
-        elif y1 - num2 < my + mw:
-            high += 1
+        elif my - mh < y1 - num2 < my + mh and x1 + num1 > mx - mw and x1 - num1 < mx + mw:
+            gretel['y'] += 10
             gretel['Hp'] = gretel['Hp'] - 60
 
-def monsters_AI():
-    global dir, high
-    x1, y1, num1, num2 = 0, 0, 0, 0
-    mx, my, mw, mh = 0, 0, 0, 0
-    for i in range(0, len(Characters) + 1):
-        if Characters[i] == 'skeleton1_1':
-            x1, y1, num1, num2 = skeleton1_1['x'], skeleton1_1['y'], skeleton1_1['width'], skeleton1_1['height']
-        elif Characters[i] == 'skeleton1_2':
-            x1, y1, num1, num2 = skeleton1_2['x'], skeleton1_2['y'], skeleton1_2['width'], skeleton1_2['height']
-        for j in range(i + 1, len(Characters) + 1):
-            if Characters[i] == 'skeleton1_1':
-                mx, my, mw, mh = skeleton1_1['x'], skeleton1_1['y'], skeleton1_1['width'], skeleton1_1['height']
-            elif Characters[i] == 'skeleton1_2':
-                mx, my, mw, mh = skeleton1_2['x'], skeleton1_2['y'], skeleton1_2['width'], skeleton1_2['height']
-            if x1 + num1 > mx - mw:
-                dir -= 1 #방향 반대로 바꾸기 class??
-            elif x1 - num1 < mx + mw:
-                dir += 1
-            elif y1 + num2 > my - mh:
-                high -= 1
-            elif y1 - num2 < my + mw:
-                high += 1
-
+# def monsters_AI():
+#     global dir, high
+#     x1, y1, num1, num2 = 0, 0, 0, 0
+#     mx, my, mw, mh = 0, 0, 0, 0
+#     for i in range(0, len(Characters) + 1):
+#         if Characters[i] == 'skeleton1_1':
+#             x1, y1, num1, num2 = skeleton1_1['x'], skeleton1_1['y'], skeleton1_1['width'], skeleton1_1['height']
+#         elif Characters[i] == 'skeleton1_2':
+#             x1, y1, num1, num2 = skeleton1_2['x'], skeleton1_2['y'], skeleton1_2['width'], skeleton1_2['height']
+#         for j in range(i + 1, len(Characters) + 1):
+#             if Characters[i] == 'skeleton1_1':
+#                 mx, my, mw, mh = skeleton1_1['x'], skeleton1_1['y'], skeleton1_1['width'], skeleton1_1['height']
+#             elif Characters[i] == 'skeleton1_2':
+#                 mx, my, mw, mh = skeleton1_2['x'], skeleton1_2['y'], skeleton1_2['width'], skeleton1_2['height']
+#             if x1 + num1 > mx - mw:
+#                 dir -= 1 #방향 반대로 바꾸기 class??
+#             elif x1 - num1 < mx + mw:
+#                 dir += 1
+#             elif y1 + num2 > my - mh:
+#                 high -= 1
+#             elif y1 - num2 < my + mw:
+#                 high += 1
+timer = False
+start, end = 0.0, 0.0
 def handle_events():
     global play
     global dir, direct, num, click, high, ax, ay, inven
+    global start, end, timer
     events = get_events()
     for event in events:
         if event.type == SDL_QUIT:
@@ -143,9 +146,10 @@ def handle_events():
                 high += 1
         if event.type == SDL_MOUSEMOTION:
             ax, ay = event.x, 600 - event.y
-        elif event.type == SDL_MOUSEMOTION and event.type == SDL_MOUSEBUTTONDOWN:
+        if event.type == SDL_MOUSEBUTTONDOWN:
+            ax, ay = event.x, 600 - event.y
             if inven == 0:
-                if event.button == SDL_BUTTON_LEFT and ax-10 < 30 and ay > 570:
+                if event.button == SDL_BUTTON_LEFT and (ax - 10 < 50 and ay > 550):
                     inven = 1
                 elif event.button == SDL_BUTTON_LEFT:
                     keys['stop'] = False
@@ -156,10 +160,8 @@ def handle_events():
                     keys['defence'] = True
                     num = 0
             elif inven == 1:
-                if event.button == SDL_BUTTON_LEFT and 590 < ax-10 < 610 and 440 < ay < 460:
+                if event.button == SDL_BUTTON_LEFT and (735 < ax-10 < 765 and 435 < ay < 465):
                     inven = 0
-
-
 
 open_canvas()
 hide_cursor()
@@ -179,13 +181,12 @@ skstop = load_image('skeleton stop.png')
 
 play = True
 keys = {'run': False, 'jump': False, 'stop': True, 'attack': False, 'defence': False}
-# button = {'inven': False}
 inven = 0
 frame = {'run': 0, 'stop': 0, 'jump': 0, 'attack': 0, 'defence': 0, 'died': 0}
 Characters = ['gretel', 'skeleton1_1', 'skeleton1_2']
-gretel = {'Hp': 110, 'x': 800//2, 'y': 150, 'width': 10, 'height': 20}
-skeleton1_1 = {'Hp': 50, 'x': 200, 'y': 200, 'width': 15, 'height': 30, 'dir': 1}
-skeleton1_2 = {'Hp': 50, 'x': 200, 'y': 300, 'width': 15, 'height': 30, 'dir': 1}
+gretel = {'Hp': 110, 'x': 800//2, 'y': 150, 'width': 15, 'height': 20}
+skeleton1_1 = {'Hp': 50, 'x': 200, 'y': 200, 'width': 20, 'height': 25, 'dir': 1}
+skeleton1_2 = {'Hp': 50, 'x': 200, 'y': 300, 'width': 20, 'height': 25, 'dir': 1}
 m1frame = {'stop': 0, 'run': 0, 'attack': 0, 'defence': 0}
 direct, dir = 1, 0
 high = 0
@@ -205,18 +206,21 @@ while play:
 
     clear_canvas()
     stage1.draw(WIDTH, HEIGHT)
-    inven_but.draw(15, 585)
     two = (two + 1) % 2
-    skstop.clip_draw(m1frame['stop'] * 150, 0, 150, 150, skeleton1_1['x'], skeleton1_1['y'])
-    skstop.clip_draw(m1frame['stop'] * 150, 0, 150, 150, skeleton1_2['x'], skeleton1_2['y'])
-    m1frame['stop'] = ((m1frame['stop']) + two) % 4
     if inven == 1:
         inventory.draw(400, 300)
         x_but.draw(750, 450)
     else:
+        inven_but.draw(25, 575)
+        skstop.clip_draw(m1frame['stop'] * 150, 0, 150, 150, skeleton1_1['x'], skeleton1_1['y'])
+        skstop.clip_draw(m1frame['stop'] * 150, 0, 150, 150, skeleton1_2['x'], skeleton1_2['y'])
+        m1frame['stop'] = (m1frame['stop'] + two) % 4
         if gretel['Hp'] < 0:
             cdied.clip_draw(frame['died'] * 100, 0, 100, 100, gretel['x'], gretel['y'])
-            frame['died'] = (frame['died'] + 1) % 7
+            frame['died'] = (frame['died'] + two) % 7
+            if frame['died'] == 0:
+                gretel['Hp'] = 110
+                gretel['x'], gretel['y'] = 400, 150
         elif keys['stop']:
             cstop.clip_draw(frame['stop'] * 100, 0, 100, 100, gretel['x'], gretel['y'])
             frame['stop'] = (frame['stop'] + 1) % 18
