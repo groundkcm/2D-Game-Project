ab60c2014cb7ee31bd7d4b232b938a3dafa59552
groundkcm <karaminseo@naver.com>
2021-11-04 03:39:16 +0900

    게임 프레임 워크 gretel 수정1

--- background.py
+++ background.py
@@ -1,8 +1,10 @@
 from pico2d import *
+from gretel import Gretel
 
 class Background:
     def __init__(self):
-        self.image = load_image('grass.png')
+        self.image = load_image('background1.png')
 
     def draw(self):
-        self.image.draw(400, 30)
+        WIDTH, HEIGHT = 1280 - Gretel().x * 2 + 160, 960 - Gretel().y * 2 + 120
+        self.image.draw(WIDTH, HEIGHT)
--- gretel.py
+++ gretel.py
@@ -10,78 +10,14 @@ key_event_table = {
     (SDL_KEYUP, SDLK_LEFT): LEFT_UP
 }
 
-
-
-# Boy States
-
-class IdleState:
-    def enter(boy, event):
-        if event == RIGHT_DOWN:
-            boy.velocity += 1
-        elif event == LEFT_DOWN:
-            boy.velocity -= 1
-        elif event == RIGHT_UP:
-            boy.velocity -= 1
-        elif event == LEFT_UP:
-            boy.velocity += 1
-        boy.timer = 1000
-
-    def exit(boy, event):
-        pass
-
-    def do(boy):
-        boy.frame = (boy.frame + 1) % 8
-        boy.timer -= 1
-
-    def draw(boy):
-        if boy.dir == 1:
-            boy.image.clip_draw(boy.frame * 100, 300, 100, 100, boy.x, boy.y)
-        else:
-            boy.image.clip_draw(boy.frame * 100, 200, 100, 100, boy.x, boy.y)
-
-class RunState:
-    def enter(boy, event):
-        if event == RIGHT_DOWN:
-            boy.velocity += 1
-        elif event == LEFT_DOWN:
-            boy.velocity -= 1
-        elif event == RIGHT_UP:
-            boy.velocity -= 1
-        elif event == LEFT_UP:
-            boy.velocity += 1
-        boy.dir = boy.velocity
-
-    def exit(boy, event):
-        pass
-
-    def do(boy):
-        boy.frame = (boy.frame + 1) % 8
-        boy.timer -= 1
-        boy.x += boy.velocity
-        boy.x = clamp(25, boy.x, 800 - 25)
-
-    def draw(boy):
-        if boy.velocity == 1:
-            boy.image.clip_draw(boy.frame * 100, 100, 100, 100, boy.x, boy.y)
-        else:
-            boy.image.clip_draw(boy.frame * 100, 0, 100, 100, boy.x, boy.y)
-
-next_state_table = {
-    IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState},
-    RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState, RIGHT_DOWN: IdleState, LEFT_DOWN: IdleState}
-}
-
-
-
-
-
+# Gretel States
 
 
 class Gretel:
 
     def __init__(self):
         self.x, self.y = 800 // 2, 90
-        self.image = load_image('animation_sheet.png')
+        self.stop = load_image('gretel stop sheet.png')
         self.dir = 1
         self.velocity = 0
         self.frame = 0
@@ -91,7 +27,7 @@ class Gretel:
         self.cur_state.enter(self, None)
 
 
-    def change_state(self,  state):
+    def change_state(self, state):
         # fill here
         pass
 
@@ -115,4 +51,62 @@ class Gretel:
 
     def handle_event(self, event):
         pass
+
+
+class IdleState:
+    def enter(gretel, event):
+        if event == RIGHT_DOWN:
+            gretel.velocity += 1
+        elif event == LEFT_DOWN:
+            gretel.velocity -= 1
+        elif event == RIGHT_UP:
+            gretel.velocity -= 1
+        elif event == LEFT_UP:
+            gretel.velocity += 1
+        gretel.timer = 1000
+
+    def exit(gretel, event):
+        pass
+
+    def do(gretel):
+        gretel.frame = (gretel.frame + 1) % 8
+        gretel.timer -= 1
+
+    def draw(gretel):
+        if gretel.dir == 1:
+            gretel.stop.clip_draw(gretel.frame * 100, 300, 100, 100, gretel.x, gretel.y)
+        else:
+            gretel.stop.clip_draw(gretel.frame * 100, 200, 100, 100, gretel.x, gretel.y)
+
+class RunState:
+    def enter(gretel, event):
+        if event == RIGHT_DOWN:
+            gretel.velocity += 1
+        elif event == LEFT_DOWN:
+            gretel.velocity -= 1
+        elif event == RIGHT_UP:
+            gretel.velocity -= 1
+        elif event == LEFT_UP:
+            gretel.velocity += 1
+        gretel.dir = gretel.velocity
+
+    def exit(gretel, event):
+        pass
+
+    def do(gretel):
+        gretel.frame = (gretel.frame + 1) % 8
+        gretel.timer -= 1
+        gretel.x += gretel.velocity
+        gretel.x = clamp(25, gretel.x, 800 - 25)
+
+    def draw(gretel):
+        if gretel.velocity == 1:
+            gretel.stop.clip_draw(gretel.frame * 100, 100, 100, 100, gretel.x, gretel.y)
+        else:
+            gretel.stop.clip_draw(gretel.frame * 100, 0, 100, 100, gretel.x, gretel.y)
+
+next_state_table = {
+    IdleState: {RIGHT_UP: RunState, LEFT_UP: RunState, RIGHT_DOWN: RunState, LEFT_DOWN: RunState},
+    RunState: {RIGHT_UP: IdleState, LEFT_UP: IdleState, RIGHT_DOWN: IdleState, LEFT_DOWN: IdleState}
+}
 
