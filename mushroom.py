085241fd74d0d29ee40d4ee7490f9f1286aa1e90
groundkcm <karaminseo@naver.com>
2021-11-25 23:27:50 +0900

    적 AI 수정2

--- mushroom.py
+++ mushroom.py
@@ -1,6 +1,8 @@
 import game_framework
 from pico2d import *
 from collision import collide
+from BehaviorTree import BehaviorTree, SelectorNode, SequenceNode, LeafNode
+import random
 import server
 import game_world
 
@@ -148,6 +150,83 @@ class Mushroom:
         self.event_que = []
         self.cur_state = IdleState
         self.cur_state.enter(self, None)
+
+
+    def prepare_patrol_points(self):
+        positions = [(43, 750), (1118, 750), (1050, 530), (575, 220), (235, 33), (575, 220), (1050, 530), (1118, 750)]
+        self.patrol_positions = []
+        for p in positions:
+            self.patrol_positions.append((p[0], 1024 - p[1]))
+
+    def wander(self):
+        self.speed = RUN_SPEED_PPS
+        self.timer -= game_framework.frame_time
+        if self.timer <= 0:
+            self.timer = 1.0
+            self.dir = random.random() * 2 * math.pi
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.RUNNING
+
+
+    def wait(self):
+        self.speed = 0
+        self.wait_timer -= game_framework.frame_time
+        if self.wait_timer <= 0:
+            self.wait_timer = 2.0
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.RUNNING
+
+
+
+    def find_player(self):
+        distance = (server.boy.x - self.x) ** 2 + (server.boy.y - self.y) ** 2
+        if distance < (PIXEL_PER_METER * 10) ** 2:
+            return BehaviorTree.SUCCESS
+        else:
+            self.speed = 0
+        return BehaviorTree.FAIL
+
+    def move_to_player(self):
+        self.speed = RUN_SPEED_PPS
+        self.dir = math.atan2(server.boy.y - self.y, server.boy.x - self.x)
+        return BehaviorTree.SUCCESS
+
+    def get_next_position(self):
+        self.target_x, self.target_y = self.patrol_positions[self.patrol_order % len(self.patrol_positions)]
+        self.patrol_order += 1
+        self.dir = math.atan2(self.target_y - self.y, self.target_x - self.x)
+        return BehaviorTree.SUCCESS
+
+    def move_to_target(self):
+        self.speed = RUN_SPEED_PPS
+        distance = (self.target_x - self.x) ** 2 + (self.target_y - self.y) ** 2
+        if distance < PIXEL_PER_METER ** 2:
+            return BehaviorTree.SUCCESS
+        else:
+            return BehaviorTree.RUNNING
+
+    def build_behavior_tree(self):
+        wander_node = LeafNode("Wander", self.wander)
+
+        wait_node = LeafNode('Wait', self.wait)
+        wander_wait_node = SequenceNode('WanderWait')
+        wander_wait_node.add_children(wander_node, wait_node)
+
+        get_next_position_node = LeafNode("Get Next Position", self.get_next_position)
+        move_to_target_node = LeafNode("Move to Target", self.move_to_target)
+        patrol_node = SequenceNode("Patrol")
+        patrol_node.add_children(get_next_position_node, move_to_target_node)
+
+        find_player_node = LeafNode("Find Player", self.find_player)
+        move_to_player_node = LeafNode("Move to Player", self.move_to_player)
+        chase_node = SequenceNode("Chase")
+        chase_node.add_children(find_player_node, move_to_player_node)
+
+        patrol_chase_node = SelectorNode("PatrolChase")
+        patrol_chase_node.add_children(chase_node, patrol_node)
+        self.bt = BehaviorTree(wander_wait_node)
 
     def get_bb(self):
         return self.x - 20, self.y - 25, self.x + 20, self.y + 20
