37e2c69fa84e91b921e0a3d536cb526d0cfd8d2f
groundkcm <karaminseo@naver.com>
2021-12-03 04:37:18 +0900

    충돌처리 수정3

--- collision.py
+++ collision.py
@@ -14,8 +14,8 @@ def collide_wall(a, b):
     left_a, bottom_a, right_a, top_a = a.get_bb()
     left_b, bottom_b, right_b, top_b = b.get_bb()
 
-    if left_a > right_b: return False
-    if right_a < left_b: return False
+    if left_a - 2 > right_b: return False
+    if right_a + 2 < left_b: return False
     if top_a + 2 < bottom_b: return False
     if bottom_a - 2 > bottom_b: return False
 
