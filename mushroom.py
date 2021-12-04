80d8b527564c20f3812cb6762a4666f1827b451c
groundkcm <karaminseo@naver.com>
2021-12-04 16:00:06 +0900

    적과 맵 충돌체크 수정4

--- mushroom.py
+++ mushroom.py
@@ -157,11 +157,11 @@ class Mushroom:
         if math.cos(self.dir) > 0:
             self.x -= self.speed * game_framework.frame_time * 10
         elif math.cos(self.dir) < 0:
-            self.x -= self.speed * game_framework.frame_time * 10
+            self.x += self.speed * game_framework.frame_time * 10
         if math.sin(self.dir) > 0:
             self.y -= self.speed * game_framework.frame_time * 10
         elif math.sin(self.dir) < 0:
-            self.y -= self.speed * game_framework.frame_time * 10
+            self.y += self.speed * game_framework.frame_time * 10
 
     def update(self):
         if self.hp <= 0:
